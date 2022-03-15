06d097a911c5acb9015145f8150f7a0d4be4ec43
edufrasson <edudomine2016@gmail.com>
2022-03-15 15:49:34 -0300

    commit

--- index.php
+++ index.php
@@ -4,11 +4,14 @@ $uri_parse = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
 include 'Controller/PessoaController.php';
 
+// :: -> operador de resolução de escopo
+// Escopo -> meio 
+
 switch($uri_parse){
     case '/pessoa':
         PessoaController::index();
     break;
-    
+
     case '/pessoa/form':
         PessoaController::form();
     break;
